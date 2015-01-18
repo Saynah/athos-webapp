495b4a19302508e324bd4516ea9d91e18f08ea3b
Mark Sena <msena505+git@gmail.com>
2015-01-17 23:12:42 -0800

    replace table with your name as var

--- app/views.py
+++ app/views.py
@@ -24,12 +24,7 @@ def cities_page_fancy():
 # page for the actual app
 @app.route("/dashboard")
 def my_dashboard():
-    with db:
-        cur = db.cursor()
-        cur.execute("SELECT Name, CountryCode, Population FROM City ORDER BY Population LIMIT 15;")
-
-        query_results = cur.fetchall()
-    cities = []
-    for result in query_results:
-        cities.append(dict(name=result[0], country=result[1], population=result[2]))
-    return render_template('cities.html', cities=cities)
+	name = 'Marruecos'
+	return render_template('dashboard.html',
+		user = {'nickname': name}
+		)
