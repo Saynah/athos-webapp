57ace0b1388b3de8bf236f7b005506d39bfd6656
Mark Sena <msena505+git@gmail.com>
2015-01-22 20:32:35 -0800

    add pie chart

--- app/views.py
+++ app/views.py
@@ -25,18 +25,38 @@ def cities_page_fancy():
 # page for the actual app
 @app.route("/dashboard")
 def my_dashboard():
+	# text input
 	name = 'Marruecos'
 
+	# line plot input
 	x = [1, 2, 3, 4, 5]
 	y = [2, 6, 2, 7, 8]
 
+	# pie chart input
+	sizes = [15, 30, 45, 10]
+	explode = (0, 0.1, 0, 0)
+	labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+
+	#
 	return render_template('dashboard.html',
 		user = {'nickname': name},
-		my_plot = html_line_plot(x,y)
+		my_plot = html_line_plot( x, y ),
+		my_pie = html_pie_chart( sizes, explode, labels )
 		)
 
 def html_line_plot(x,y):
 	# mpld3 to create line plot 
 	fig, ax = plt.subplots()
 	ax.plot(x, y, 'k-')
+	return mpld3.fig_to_html(fig)
+
+def html_pie_chart(sizes, explode, labels):
+	# mpld3 to create bar chart
+	colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+
+	fig, ax = plt.subplots()
+	ax.pie(sizes, explode=explode, labels=labels, colors=colors,
+	        autopct='%1.1f%%', shadow=True, startangle=90)
+	# Set aspect ratio to be equal so that pie is drawn as a circle.
+	ax.axis('equal')
 	return mpld3.fig_to_html(fig)
